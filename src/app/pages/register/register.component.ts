486d08948169d6f66bf9cd5852f30f7eb588c92e
Gulshan Anand <gulshan2052@gmail.com>
2025-05-18 12:20:31 +0530

    register form includes confirmPassword

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule, AbstractControl, ValidationErrors } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
 import { RegisterData } from '../../interfaces/registerData.interface';
@@ -19,29 +19,36 @@ export class RegisterComponent {
       firstname: ['', [Validators.required]],
       lastname: ['', []],
       email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(6)]]
-    });
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', Validators.required]
+    }, { validators: this.passwordsMatchValidator });
+  }
+
+  passwordsMatchValidator(form: AbstractControl): ValidationErrors | null {
+    const password = form.get('password')?.value;
+    const confirmPassword = form.get('confirmPassword')?.value;
+    return password === confirmPassword ? null : { passwordsMismatch: true };
   }
 
   onSubmit() {
-      if (this.registerForm.valid) {
-        console.log('Form Submitted', this.registerForm.value);
-        const registerData: RegisterData = this.registerForm.value;
-        registerData.role = "USER";
-  
-        this.authService.register(registerData).subscribe(
-          (response) => {
-            localStorage.setItem('token', response.token);
-            alert('Register successful!');
-            this.router.navigate(['/home']);
-          },
-          (error) => {
-            console.log(error);
-            alert(error.error.message);
-          }
-        );
-      } else {
-        alert('Invalid Details');
-      }
+    if (this.registerForm.valid) {
+      console.log('Form Submitted', this.registerForm.value);
+      const registerData: RegisterData = this.registerForm.value;
+      registerData.role = "USER";
+
+      this.authService.register(registerData).subscribe(
+        (response) => {
+          localStorage.setItem('token', response.token);
+          alert('Register successful!');
+          this.router.navigate(['/home']);
+        },
+        (error) => {
+          console.log(error);
+          alert(error.error.message);
+        }
+      );
+    } else {
+      alert('Invalid Details');
     }
+  }
 }
