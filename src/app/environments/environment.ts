ef3463836efcb41fac44724bf823e8456e68a869
Gulshan Anand <gulshan2052@gmail.com>
2025-08-17 21:56:53 +0530

    backend url added to environment.ts file

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    baseUrl: 'http://localhost:8080',
+    baseUrl: 'http://shortify-service:8080',
     apiPath: '/api/v1',
     featureFlag: true,
     appName: 'Shortify'
