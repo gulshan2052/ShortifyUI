a99731a0b6bf1d5d61224abe3132b921c8ec9958
Gulshan Anand <gulshan2052@gmail.com>
2025-08-17 21:35:52 +0530

    prod service url changed

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    baseUrl: 'http://localhost:8080',
+    baseUrl: 'http://shortify-service:8080',
     apiPath: '/api/v1',
     featureFlag: false,
     appName: 'Shortify'
